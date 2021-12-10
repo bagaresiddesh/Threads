85d975325d28eb883572ef6353a5409839a0f4a3
siddesh bagare <siddeshbagare@gmail.com>
2021-12-10 16:12:36 +0530

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using SimpleFunctions;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Functions obj = new Functions();
+
+            Console.WriteLine("\nEven numbers upto 1000 : \n");
+            obj.Even();
+
+            Console.WriteLine("\nPallindrome numbers upto 1000 : \n");
+            obj.Pallindrome();
+
+            Console.WriteLine("\nEven numbers upto 1000 using double Thread : \n");
+            Thread thread2 = new Thread(new ThreadStart(obj.Even));
+            thread2.Start();
+
+            Console.WriteLine("\nPallindrome numbers upto 1000 using double Thread : \n");
+            Thread thread3 = new Thread(new ThreadStart(obj.Pallindrome));
+            thread3.Start();
+
+            Console.WriteLine("\nPallindrome numbers upto 1000 using single Thread : \n");
+            Thread thread1 = new Thread(new ThreadStart(obj.Pallindrome));
+            thread1.Start();
+            obj.Even();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimpleFunctions/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleFunctions
+{
+    public class Functions
+    {
+        public void Even()//Even Function 
+        {
+            int start = 1, end = 1000;
+
+            for(int i=start;i<=end;i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + "\t");
+                    Thread.Sleep(10);
+                }
+            }         
+        }
+       
+        public void Pallindrome()//Pallindrome Function 
+        {
+            int start = 1, end = 1000, digit = 0, temp=0, reverse;
+
+            for (int i = start; i <= end; i++)
+            {
+                temp = i;
+                reverse = 0;
+
+                while(temp!=0)
+                {
+                    digit = temp % 10;
+                    reverse = reverse * 10 + digit;
+                    temp = temp / 10;
+                    
+                }
+
+                if(i==reverse)
+                {
+                    Console.Write(reverse+"*\t");
+                    Thread.Sleep(10);
+                }
+            }
+        }
+    }
+}
